072192647746a5b9c7350c2e725b4f3da84bc6de
yanjie <42068140+yanjie0916@users.noreply.github.com>
2018-12-23 00:46:37 +0800

    遊戲角色畫面

---
+++ retailer.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> R1 零售商</title>
+</head>
+
+<body>
+    <H1>Retailer</H1>
+	<form>
+		<p>目前是第1周</p>
+        <p>消費者要求啤酒數量：6</p>
+		請輸入本週訂購的啤酒數量:<input type="text" name="num beer">
+		<input type="submit" value="Submit">
+	</form>
+	<hr>
+    <table style="width: 100%">
+	    <tbody><tr>
+		    <td>週次</td>
+		    <td>到貨量</td>
+		    <td>庫存量</td>
+		    <td>出貨量</td>
+	    	<td>訂貨量</td>
+		    <td>在途量</td>
+		    <td>當期成本</td>
+		    <td>累計成本</td>
+		    <td>需求量</td>
+
+	    </tr>
+        <tr>
+            <td>1</td>
+            <td>4</td>
+            <td>15</td>
+            <td>6</td>
+            <td>4</td>
+            <td>16</td>
+            <td>8</td>
+            <td>8</td>
+            <td>4</td>
+        </tr>
+    </table>
+	<hr>
+</body>
+</html>
