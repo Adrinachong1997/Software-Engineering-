f1b60f954dd4672549473e932a40cc56cbbb333c
yanjie0916 <s105213062@mail1.ncnu.edu.tw>
2018-12-31 17:44:09 +0800

    取得period資料庫的週次和gamecycle目前週次的消費者要去

--- retailer.php
+++ retailer.php
@@ -12,22 +12,29 @@ require("playerModel.php");
 <body>
     <H1>Retailer</H1>
 	<form method="post" action="orderControl.php">
-	<input type = "hidden" name="order" value="updateweek"/>
-		<p>目前是第<?php //sumbit后week+1
-		$result= weeklist();
-		while($rs=mysqli_fetch_assoc($result)){
-		echo $rs['no'];
-	}
-	?>周</p>
-        <p>消費者要求啤酒數量：</p>
+	<input type = "hidden" name="operation" value="updateweek"/>
+		<p>目前是第
+		<?php //從period撈目前週次
+			$result= getCurrentPeriod();
+			while($rs=mysqli_fetch_assoc($result)){
+				echo $rs['no'];
+			}
+		?>
+		周</p>
+		<p>消費者要求啤酒數量：<?php //從gamecycle撈目前的消費者要去
+		$result= getCurrentDemand();
+		if($rs=mysqli_fetch_assoc($result)){
+			echo $rs['setno'];
+		}
+		?></p>
 		請輸入本週訂購的啤酒數量:<?php?>
-		<input type = "hidden" name="order" value="update"/>
-		<input type="text" name="sumbit">
-		<input type="submit"  value=" 下單 " />
+		<input type ="hidden" name="operation" value="update"/>
+		<input type ="text" name="order">
+		<input type ="submit" value=" 下單 " />
 		 
 	</form>
 	<form method="post" action="orderControl.php">
-	<input type = "hidden" name="order" value="reset"/>
+	<input type = "hidden" name="operation" value="reset"/>
     <input type = "submit" value = "重置"> 
 </form>
 	<hr>
@@ -45,17 +52,20 @@ require("playerModel.php");
 
 	    </tr>
 		<?php
-		$result = orderList();
+		$result = getOrderList();
 		while (	$rs = mysqli_fetch_assoc($result)) {
-			echo "<tr><td>" , $rs['week'] ,
-			"</td><td>" , $rs['original_stock'],
-			"</td><td>" , $rs['expected_arrival'],
-			"</td><td>" , $rs['actual_arrival'],
-			"</td><td>" , $rs['orders'],
-			"</td><td>" , $rs['cost'],
-			"</td><td>" , $rs['acc_cost'],
-			"</td><td>" , $rs['demand'],
-			"</td><td>" , $rs['actual_shipment'];
+			echo 
+			"<tr>",
+			"<td>" , $rs['week'] , "</td>",
+			"<td>" , $rs['original_stock'], "</td>",
+			"<td>" , $rs['expected_arrival'], "</td>",
+			"<td>" , $rs['actual_arrival'], "</td>",
+			"<td>" , $rs['orders'], "</td>",
+			"<td>" , $rs['cost'], "</td>",
+			"<td>" , $rs['acc_cost'], "</td>",
+			"<td>" , $rs['demand'], "</td>",
+			"<td>" , $rs['actual_shipment'], "</td>",
+			"</tr>";
 		
 		}
 			?>
