d56addd09ab1efcfd6e3d995baac878d1afc6f5b
yanjie0916 <s105213062@mail1.ncnu.edu.tw>
2018-12-31 14:39:26 +0800

    增加清除period table 功能 和撈period的no週次

--- playerModel.php
+++ playerModel.php
@@ -1,19 +1,34 @@
 <?php
 require("dbconfig.php");
-function updateweek($sumbit,$week) 
+function updateweek() 
+{
+    if($no>0){
+    global $db;
+    $sql = " UPDATE `period` SET `no` = no+1 where id = 1";
+	$stmt = mysqli_prepare($db, $sql);
+	mysqli_stmt_bind_param($stmt, "i",$id);
+    mysqli_stmt_execute($stmt); 
+    return ;
+    }
+}
+function weeklist() 
 {
     global $db;
-    $sql = "UPDATE `period` SET `week` = ? period.id = 1";
-	$stmt = mysqli_prepare($db, $sql);
-	mysqli_stmt_bind_param($stmt, "i",$sumbit);
+    $sql = "select * from period ";
+    $stmt = mysqli_prepare($db, $sql);
+    //mysqli_stmt_bind_param($stmt, "ss", $id, $pwd);
+    mysqli_stmt_execute($stmt); //執行SQL
+    $result = mysqli_stmt_get_result($stmt); 
+    return $result;
+}
+function r_period()
+{
+    global $db;
+    $sql = "TRUNCATE TABLE period";
+    $stmt = mysqli_prepare($db, $sql);
     mysqli_stmt_execute($stmt); 
-	return ;
-    
-    /*if(isset($_POST['submit'])){
-        $sql = "select * from period where week";
-        $stmt = mysqli_prepare($db, $sql);
-        //mysqli_stmt_bind_param($stmt, "ss", $id, $pwd);
-        mysqli_stmt_execute($stmt); //執行SQL
-        $result = mysqli_stmt_get_result($stmt); 
-    }*/
+    $sql = "INSERT INTO period(`id`,`no`) values (1,0);";
+	$stmt = mysqli_prepare($db, $sql);
+	mysqli_stmt_execute($stmt); 
+	return;
 }
