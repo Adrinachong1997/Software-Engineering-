96000f68a40cd89078fc97ed7187be823a61e9ba
yanjie0916 <s105213062@mail1.ncnu.edu.tw>
2019-01-02 22:13:28 +0800

    縮排更正

--- retailer.php
+++ retailer.php
@@ -14,6 +14,7 @@ $serno=1;
 
 <body>
     <H1>Retailer</H1>
+
 	<form method="post" action="orderControl.php">
 		<input type = "hidden" name="pid" value=<?php echo $pid ?>/>
 		<input type = "hidden" name="serno" value=<?php echo $serno ?>/>
@@ -50,36 +51,39 @@ $serno=1;
 	</form>
 	<hr>
     <table style="width: 100%">
-	    <tbody><tr>
-			<td>週次</td>
-			<td>庫存</td>
-		    <td>預期到貨</td>
-		    <td>實際到貨</td>
-			<td>訂單</td>
-			<td>成本</td>
-			<td>累積成本</td>
-			<td>需求</td>
-			<td>實際出貨</td>
-
-	    </tr>
-		<?php
-		$result = getOrderList($serno,$pid);
-		while (	$rs = mysqli_fetch_assoc($result)) {
-			echo 
-			"<tr>",
-			"<td>" , $rs['week'] , "</td>",
-			"<td>" , $rs['original_stock'], "</td>",
-			"<td>" , $rs['expected_arrival'], "</td>",
-			"<td>" , $rs['actual_arrival'], "</td>",
-			"<td>" , $rs['orders'], "</td>",
-			"<td>" , $rs['cost'], "</td>",
-			"<td>" , $rs['acc_cost'], "</td>",
-			"<td>" , $rs['demand'], "</td>",
-			"<td>" , $rs['actual_shipment'], "</td>",
-			"</tr>";
-		
-		}
+	    <thead>
+			<tr>
+				<td>週次</td>
+				<td>庫存</td>
+				<td>預期到貨</td>
+				<td>實際到貨</td>
+				<td>訂單</td>
+				<td>成本</td>
+				<td>累積成本</td>
+				<td>需求</td>
+				<td>實際出貨</td>
+	    	</tr>
+		</thead>
+		<tbody>
+			<?php
+				$result = getOrderList($serno,$pid);
+				while (	$rs = mysqli_fetch_assoc($result)) {
+					echo 
+					"<tr>",
+					"<td>" , $rs['week'] , "</td>",
+					"<td>" , $rs['original_stock'], "</td>",
+					"<td>" , $rs['expected_arrival'], "</td>",
+					"<td>" , $rs['actual_arrival'], "</td>",
+					"<td>" , $rs['orders'], "</td>",
+					"<td>" , $rs['cost'], "</td>",
+					"<td>" , $rs['acc_cost'], "</td>",
+					"<td>" , $rs['demand'], "</td>",
+					"<td>" , $rs['actual_shipment'], "</td>",
+					"</tr>";
+				
+				}
 			?>
+		</tbody>
     </table>
 </body>
 </html>
