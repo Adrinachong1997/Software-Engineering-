b167e10d28bf181ea2f1ee000f81cb9c16bafb42
yanjie0916 <s105213062@mail1.ncnu.edu.tw>
2019-01-02 22:05:28 +0800

    更改wholesaler的界面 增加連接order,playerModel裡面的function

--- wholesaler.php
+++ wholesaler.php
@@ -1,27 +1,51 @@
 <?php
 require("dbconfig.php");
-$sql = "select * from player_record,gamecycle";
-$stmt = mysqli_prepare($db, $sql);
-mysqli_stmt_execute($stmt); //執行SQL
-$result = mysqli_stmt_get_result($stmt);
-
-if ($rs=mysqli_fetch_assoc($result)) {
-	$no=$rs['no'];  
-	$setno=$rs['setno'];
-}  
+require("orderModel.php");
+require("playerModel.php");
+global $db;
+$pid=3;
+$serno=1;
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title> 批發商</title>
+	<title> 批发商</title>
 </head>
 
 <body>
     <H1>Wholesaler</H1>
-	<form>
-		<p>目前是第<?php echo $no; ?>周</p>
-		請輸入本週訂購的啤酒數量:<input type="text" name="num beer">
-		<input type="submit" value="Submit">
+	<form method="post" action="orderControl.php">
+		<input type = "hidden" name="pid" value=<?php echo $pid ?>/>
+		<input type = "hidden" name="serno" value=<?php echo $serno ?>/>
+		<input type = "hidden" name="operation" value="update"/>
+		<p>目前是第
+		<?php //從period撈目前週次
+			$result= getCurrentPeriod();
+			// 判断四个同时一起按submit，TODO
+			while($rs=mysqli_fetch_assoc($result)){
+				$currWeek = $rs['week']+1;
+				echo $currWeek;
+			}
+		?>
+		周
+		</p>
+		<p>消費者上週要求的啤酒數量：
+		<?php //從gamecycle撈目前的消費者要去
+			$result= getCurrentDemand($pid,$currWeek);
+			if($rs=mysqli_fetch_assoc($result)){
+				echo $rs['demand'];
+			}
+		?>
+		</p>
+		請輸入本週訂購的啤酒數量:<?php ?>
+		<input type = "hidden" name="week" value="<?php echo $currWeek ?>"/>
+		<input type ="text" name="order">
+		<input type ="submit" value=" 下單 " />
+			
+	</form>
+	<form method="post" action="orderControl.php">
+		<input type = "hidden" name="operation" value="reset"/>
+		<input type = "submit" value = "重置"> 
 	</form>
 	<hr>
     <table style="width: 100%">
@@ -38,21 +62,21 @@ if ($rs=mysqli_fetch_assoc($result)) {
 
 	    </tr>
 		<?php
-		$sql = "select * from player_record where cid =3";
-		$stmt = mysqli_prepare($db, $sql);
-		mysqli_stmt_execute($stmt); //執行SQL
-		$result = mysqli_stmt_get_result($stmt);
+		$result =getOrderList($serno,$pid);
 		while (	$rs = mysqli_fetch_assoc($result)) {
-			echo "<tr><td>" , $rs['week'] ,
-			"</td><td>" , $rs['original_stock'],
-			"</td><td>" , $rs['expected_arrival'],
-			"</td><td>" , $rs['actual_arrival'],
-			"</td><td>" , $rs['orders'],
-			"</td><td>" , $rs['cost'],
-			"</td><td>" , $rs['acc_cost'],
-			"</td><td>" , $rs['demand'],
-			"</td><td>" , $rs['actual_shipment'];
-			break;
+			echo 
+			"<tr>",
+			"<td>" , $rs['week'] , "</td>",
+			"<td>" , $rs['original_stock'], "</td>",
+			"<td>" , $rs['expected_arrival'], "</td>",
+			"<td>" , $rs['actual_arrival'], "</td>",
+			"<td>" , $rs['orders'], "</td>",
+			"<td>" , $rs['cost'], "</td>",
+			"<td>" , $rs['acc_cost'], "</td>",
+			"<td>" , $rs['demand'], "</td>",
+			"<td>" , $rs['actual_shipment'], "</td>",
+			"</tr>";
+		
 		}
 			?>
     </table>
