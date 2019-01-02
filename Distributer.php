fdbc22e02579fd23c95aa623f770486f9d627349
yanjie0916 <s105213062@mail1.ncnu.edu.tw>
2019-01-02 22:19:08 +0800

    套用模板

--- Distributer.php
+++ Distributer.php
@@ -1,14 +1,10 @@
 <?php
 require("dbconfig.php");
-$sql = "select * from player_record,gamecycle";
-$stmt = mysqli_prepare($db, $sql);
-mysqli_stmt_execute($stmt); //執行SQL
-$result = mysqli_stmt_get_result($stmt);
-
-if ($rs=mysqli_fetch_assoc($result)) {
-	$no=$rs['no'];  
-	$setno=$rs['setno'];
-}  
+require("orderModel.php");
+require("playerModel.php");
+global $db;
+$pid=2;
+$serno=1;
 ?>
 <!DOCTYPE html>
 <html>
@@ -18,11 +14,7 @@ if ($rs=mysqli_fetch_assoc($result)) {
 
 <body>
     <H1>Distributer</H1>
-	<form>
-		<p>目前是第<?php echo $no; ?>周</p>
-		請輸入本週訂購的啤酒數量:<input type="text" name="num beer">
-		<input type="submit" value="Submit">
-	</form>
+	<?php include("formTemplate.php"); ?>
 	<hr>
     <table style="width: 100%">
 	    <tbody><tr>
@@ -38,21 +30,21 @@ if ($rs=mysqli_fetch_assoc($result)) {
 
 	    </tr>
 		<?php
-		$sql = "select * from player_record where cid =2";
-		$stmt = mysqli_prepare($db, $sql);
-		mysqli_stmt_execute($stmt); //執行SQL
-		$result = mysqli_stmt_get_result($stmt);
+		$result =getOrderList($serno,$pid);
 		while (	$rs = mysqli_fetch_assoc($result)) {
-			echo "<tr><td>" , $rs['week'] ,
-			"</td><td>" , $rs['original_stock'],
-			"</td><td>" , $rs['expected_arrival'],
-			"</td><td>" , $rs['actual_arrival'],
-			"</td><td>" , $rs['orders'],
-			"</td><td>" , $rs['cost'],
-			"</td><td>" , $rs['acc_cost'],
-			"</td><td>" , $rs['demand'],
-			"</td><td>" , $rs['actual_shipment'];
-			break;
+			echo 
+			"<tr>",
+			"<td>" , $rs['week'] , "</td>",
+			"<td>" , $rs['original_stock'], "</td>",
+			"<td>" , $rs['expected_arrival'], "</td>",
+			"<td>" , $rs['actual_arrival'], "</td>",
+			"<td>" , $rs['orders'], "</td>",
+			"<td>" , $rs['cost'], "</td>",
+			"<td>" , $rs['acc_cost'], "</td>",
+			"<td>" , $rs['demand'], "</td>",
+			"<td>" , $rs['actual_shipment'], "</td>",
+			"</tr>";
+		
 		}
 			?>
     </table>
--- Factory.php
+++ Factory.php
@@ -1,29 +1,20 @@
 <?php
 require("dbconfig.php");
-require_once("orderModel.php");
-$sql = "select * from player_record,gamecycle";
-$stmt = mysqli_prepare($db, $sql);
-mysqli_stmt_execute($stmt); //執行SQL
-$result = mysqli_stmt_get_result($stmt);
-
-if ($rs=mysqli_fetch_assoc($result)) {
-	$no=$rs['no'];  
-	$setno=$rs['setno'];
-}  
+require("orderModel.php");
+require("playerModel.php");
+global $db;
+$pid=1;
+$serno=1;
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title> 工廠</title>
+	<title>工廠</title>
 </head>
 
 <body>
     <H1>Factory</H1>
-	<form>
-		<p>目前是第<?php echo $no; ?>周</p>
-		請輸入本週訂購的啤酒數量:<input type="text" name="num beer">
-		<input type="submit" value="Submit">
-	</form>
+	<?php include("formTemplate.php"); ?>
 	<hr>
     <table style="width: 100%">
 	    <tbody><tr>
@@ -39,18 +30,21 @@ if ($rs=mysqli_fetch_assoc($result)) {
 
 	    </tr>
 		<?php
-		$result = orderList();
+		$result =getOrderList($serno,$pid);
 		while (	$rs = mysqli_fetch_assoc($result)) {
-			echo "<tr><td>" , $rs['week'] ,
-			"</td><td>" , $rs['original_stock'],
-			"</td><td>" , $rs['expected_arrival'],
-			"</td><td>" , $rs['actual_arrival'],
-			"</td><td>" , $rs['orders'],
-			"</td><td>" , $rs['cost'],
-			"</td><td>" , $rs['acc_cost'],
-			"</td><td>" , $rs['demand'],
-			"</td><td>" , $rs['actual_shipment'];
-			break;
+			echo 
+			"<tr>",
+			"<td>" , $rs['week'] , "</td>",
+			"<td>" , $rs['original_stock'], "</td>",
+			"<td>" , $rs['expected_arrival'], "</td>",
+			"<td>" , $rs['actual_arrival'], "</td>",
+			"<td>" , $rs['orders'], "</td>",
+			"<td>" , $rs['cost'], "</td>",
+			"<td>" , $rs['acc_cost'], "</td>",
+			"<td>" , $rs['demand'], "</td>",
+			"<td>" , $rs['actual_shipment'], "</td>",
+			"</tr>";
+		
 		}
 			?>
     </table>
