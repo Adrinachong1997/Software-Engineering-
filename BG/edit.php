08c5f6d070414b18a2b6b1d6ce61a09520c3f250
nwgosick0523 <45961770+nwgosick0523@users.noreply.github.com>
2019-01-03 01:24:45 +0800

    css美化

--- BG/edit.php
+++ BG/edit.php
@@ -1,41 +1,76 @@
 <?php
-require_once("dbconfig.php");
-//checkLogin();
+    require_once("dbconfig.php");
+    //checkLogin();
 ?>
 <!DOCTYPE html>
-<html>
-<body>
-<p>Edit your data </p>
-<?php
-require("userModel.php");
-$id=getCurrentUser();
-$img_name=pic_name($id);
-	$sql = "select * from user where id=?;";
-	$stmt = mysqli_prepare($db, $sql );
-	mysqli_stmt_bind_param($stmt, "s", $id);
-	mysqli_stmt_execute($stmt);
-	$result = mysqli_stmt_get_result($stmt); 
-    
-    if ($rs=mysqli_fetch_array($result)) {
-        
-?>
-<form method="post" action="updateuser.php" enctype="multipart/form-data">
-  <tr>
-  <td><label>
-      User ID: <input type="hidden" name="id" value="<?php echo $rs['id']; ?>" />
-	  <?php echo $rs['id']; ?><br />
-    </label></td>
-    <td><label>
-      Password : <input name="password" type="text" id="password" value="<?php echo $rs['password']; ?>" required /><br />
-    </label></td>
-    <img src="uploadfiles/<?php echo $img_name;?>.png" width="200" height="200"/>
-    <td><input type="file" name="upload" id="upload" accept=".gif,.jpeg,.pjpeg,.png"/><br /></td>
-  </tr>
-<input type="submit" name="Submit" value="送出" />
-</form>
+    <html>
+        <head>
+            <link rel="stylesheet" type="text/css" href="style.css" />
+            <meta charset="UTF-8"/>
+            <title>編輯用戶資料</title>
+            <style type="text/css">
+            </style>
+            <script type="text/javascript">
+            </script>
+        </head>
+        <body>
+        <table id="main">
+            <form id="team" method="post" action="updateuser.php" enctype="multipart/form-data">
+                <tr>
+                    <td colspan="2" id="background"><font size="6">編輯用戶資料</font></td>
+                </tr>
+                <?php
+                    require("userModel.php");
+                    $id=getCurrentUser();
+                    $img_name=pic_name($id);
+                    $sql = "select * from user where id=?;";
+                    $stmt = mysqli_prepare($db, $sql );
+                    mysqli_stmt_bind_param($stmt, "s", $id);
+                    mysqli_stmt_execute($stmt);
+                    $result = mysqli_stmt_get_result($stmt); 
+                        
+                    if ($rs=mysqli_fetch_array($result)) {
+                            
+                ?>
+                <tr>
+                    <td>
+                        <label>用戶名稱 : <input type="hidden" name="id" value="<?php echo $rs['id']; ?>" />
+                    </td>
+                    <td>
+                        <?php echo $rs['id']; ?><br /></label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>新密碼 : 
+                    </td>
+                    <td>
+                        <input name="password" type="text" id="password" value="<?php echo $rs['password']; ?>" required /><br />
+                        </label>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        目前個人照片</br></br><img src="uploadfiles/<?php echo $img_name;?>.png" width="200" height="300"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>更新個人照片 : 
+                    </td>
+                    <td >
+                        <input type="file" name="upload" id="upload" accept=".gif,.jpeg,.pjpeg,.png"/><br />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <input type="submit" name="Submit" value="送出" />
+                    </td>
+                </tr>
+        </form>
 
 <?php
-} else echo "cannot find your data to edit.";
+    } else echo "cannot find your data to edit.";
 
 ?>
 </body>
