8aa1f0d8f191f098e91e6cf93da0ba31b6c322b5
yanjie0916 <s105213062@mail1.ncnu.edu.tw>
2019-01-02 22:06:41 +0800

    讓四個角色的檔案共用一個control檔案。

--- orderControl.php
+++ orderControl.php
@@ -1,25 +1,36 @@
 <?php
+error_reporting(E_ERROR | E_PARSE);
 require_once("orderModel.php");
 require_once("playerModel.php");
+$pname = array(
+    '1' => "factory.php",
+    '2' => "Distributer.php",
+    '3' => "wholesaler.php",
+    '4' => "retailer.php");
+$serno = (int)$_POST["serno"];
+$pid = (int)$_POST["pid"];
 $operation = $_POST["operation"];
 $order = (int)$_POST["order"];
-$no=(int)$_REQUEST["no"];
+$week = (int)$_REQUEST["week"];
 switch($operation){
     case "reset":
-        r_playerrecord();
         r_period();
+        r_playerrecord($serno, $pid);
         break;
     case "update":
         updatePeriod();
-        updateOrder($order);
-        getCurrentDemand();
-        updateactual_shipment($order);
+        insertOrder($serno,$pid,$order,$week);
+        getCurrentDemand($pid,$week);
+        //updatexpected_arrival($serno,$cid,$week);
+        //updateactual_shipment($order);
         //updateexpected_arrival($order,$week);
         break;
-    case "updateweek":
-        updateweek();
-        break;
+    
 }
-header("Location: retailer.php");
+if($pid != ''){
+    header("Location: ".$pname[$pid]);
+} else {
+    echo '<script>javascript:history.go(-1)</script>';
+}
 
 ?>
