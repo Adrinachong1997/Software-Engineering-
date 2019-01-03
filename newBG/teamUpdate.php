fcf4a2f6629133916149cc4a3ceb2c513f197201
nwgosick0523 <45961770+nwgosick0523@users.noreply.github.com>
2019-01-03 22:29:27 +0800

    建立function簡化程式碼

--- newBG/teamUpdate.php
+++ newBG/teamUpdate.php
@@ -14,68 +14,9 @@
 		<table id="main">
             <form id="team" method="post" accept-charset="utf-8">
 				<?php
-                require('dbconfig.php');
-                require('userModel.php');
-				
-                $id=getCurrentUser();
-
-                $role = $_POST['role'];
-				$_SESSION['role'] = $_POST['role'];
-				
-				$uid = $id ;//$rs['id'];
-
-				$str_sec = explode(";",$_SESSION['role']);
-				    
-				echo '<tr><td colspan="2" id="background"><font size="6">成功加入隊伍</font></td>',
-					"</tr><tr><td>",
-                 "隊伍名稱</td><td>$str_sec[1]</td></tr>",
-            
-				print_r($str_sec);
-                
-
-				if ($str_sec[0] == 1) {
-                    echo '<tr><td>角色</td><td>工廠</td></tr>';
-					$sql = "update tgame set r1 = ? where tname = ? ";
-					$stmt = mysqli_prepare($db, $sql);
-					mysqli_stmt_bind_param($stmt, "ss",$uid,$str_sec[1]);
-                    mysqli_stmt_execute($stmt); //執行SQL
-                    $result = mysqli_stmt_get_result($stmt); 
-                 
-
-				} 
-
-				else if ($str_sec[0] == 2) {
-                    echo '<tr><td>角色</td><td>大盤商</td></tr>';
-					$sql = "update tgame set r2 = ? where tname = ? ";
-					$stmt = mysqli_prepare($db, $sql);
-					mysqli_stmt_bind_param($stmt, "ss",$uid,$str_sec[1]);
-					mysqli_stmt_execute($stmt); //執行SQL
-                    $result = mysqli_stmt_get_result($stmt); 
-              
-				} 
-
-				else if($str_sec[0] == 3) {
-                    echo '<tr><td>角色</td><td>批發商</td></tr>';
-					$sql = "update tgame set r3 = ? where tname = ? ";
-					$stmt = mysqli_prepare($db, $sql);
-					mysqli_stmt_bind_param($stmt, "ss",$uid,$str_sec[1]);
-					mysqli_stmt_execute($stmt); //執行SQL
-                    $result = mysqli_stmt_get_result($stmt); 
-                  
-				} 
-
-				else if ($str_sec[0] == 4) {	
-                    echo '<tr><td>角色</td><td>零售商</td></tr>';
-					$sql = "update tgame set r4 = ? where tname = ? ";
-					$stmt = mysqli_prepare($db, $sql);
-					mysqli_stmt_bind_param($stmt, "ss",$uid,$str_sec[1]);
-					mysqli_stmt_execute($stmt); //執行SQL
-                    $result = mysqli_stmt_get_result($stmt); 
-                  
-				} 
-				else {
-					echo "cannot update!";
-				}
+					//加入隊伍
+					require("gameModel.php");
+					updateTeam();
 				?>
                 <tr>
                     <td colspan="2"><img src="1.gif" width="200" height="200"/></br>等待遊戲開始</td>
