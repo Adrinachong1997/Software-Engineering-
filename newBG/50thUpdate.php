8d2bc21363d0a0a2480884e574c11b0bc156be7b
nwgosick0523 <45961770+nwgosick0523@users.noreply.github.com>
2019-01-03 22:27:04 +0800

    建立function簡化程式碼

--- newBG/50thUpdate.php
+++ newBG/50thUpdate.php
@@ -6,34 +6,15 @@
         <meta name="keywords" content=" " />
         <meta name="description" content=" " />
         <title>五十期資料設定</title>
-        
         <style type="text/css">
         </style>
     </head>
 	<body>
         <?php
-        require('dbconfig.php');
-        //$no=$_POST['no'];
-        $setno=$_POST['setno'];
-        $sqlData=0;
-        //把50期需求存入資料庫
-        if ($db) {echo'<table id="main" method="post" >',
-            '<tr><td colspan="2">',
-                '<font size="6">資料已新增</font>',
-            '</td></tr>';
-            
-            for($i = 0 ; $i < 50 ; $i++) {
-                $sqlData = $setno[$i];
-                $no = $i + 1;
-                $sql = "insert into gamecycle (no, setno) values (?,?)";
-                $stmt = mysqli_prepare($db, $sql); //prepare sql statement
-                mysqli_stmt_bind_param($stmt, "ii", $no, $sqlData); //bind parameters with variables
-                mysqli_stmt_execute($stmt);  //執行SQL
-            }
-        } else {
-            echo 'empty title, cannot insert.';
-        }
-        
+            require('dbconfig.php');
+            require('gameModel.php');
+            //把50期需求存入資料庫
+            ftyUpdate();
         ?>	
         <!--啟動遊戲的按鈕 -->
         <table id="main">
