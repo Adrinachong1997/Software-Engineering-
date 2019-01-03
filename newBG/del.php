ed9d1d8d0383b8f1937160b272bbebb39834ebd5
NW <s105213078@mail1.ncnu.edu.tw>
2019-01-03 23:03:49 +0800

    test git

--- newBG/del.php
+++ newBG/del.php
@@ -8,6 +8,5 @@ $sqlData=0;
             $stmt = mysqli_prepare($db, $sql );
             mysqli_stmt_bind_param($stmt, "i", $id);
             mysqli_stmt_execute($stmt);
-        
-
+        ///s
 ?>
