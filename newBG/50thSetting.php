89c3ee7f25310fd5f54087812343a7cfa4dd2514
nwgosick0523 <chinglung0523@gmail.com>
2019-01-04 10:31:58 +0800

    50期欄位調整

--- newBG/50thSetting.php
+++ newBG/50thSetting.php
@@ -23,97 +23,97 @@
     </head>
     <body>
     <table id="main">
-            <form id="setno" method="post" action="50thUpdate.php" accept-charset="utf-8">
+            <form id="demand" method="post" action="50thUpdate.php" accept-charset="utf-8">
                 <tr>
                     <td colspan="5" id="background"><font size="6">五十期資料設定</font></td>
                 </tr>       
                 <tr>                   
-                    <td colspan="1">1<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>       
-                    <td colspan="1">2<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>                
-                    <td colspan="1">3<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>                
-                    <td colspan="1">4<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>         
-                    <td colspan="1">5<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>
+                    <td colspan="1">1<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>       
+                    <td colspan="1">2<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>                
+                    <td colspan="1">3<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>                
+                    <td colspan="1">4<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>         
+                    <td colspan="1">5<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>
                 </tr>
                 <tr>
                 </tr>
                 <tr>                   
-                    <td colspan="1">6<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>       
-                    <td colspan="1">7<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>                
-                    <td colspan="1">8<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>                
-                    <td colspan="1">9<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>         
-                    <td colspan="1">10<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>
+                    <td colspan="1">6<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>       
+                    <td colspan="1">7<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>                
+                    <td colspan="1">8<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>                
+                    <td colspan="1">9<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>         
+                    <td colspan="1">10<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>
                 </tr>
                 <tr>
                 </tr>
                 <tr>                   
-                    <td colspan="1">11<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>       
-                    <td colspan="1">12<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>                
-                    <td colspan="1">13<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>                
-                    <td colspan="1">14<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>         
-                    <td colspan="1">15<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>
+                    <td colspan="1">11<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>       
+                    <td colspan="1">12<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>                
+                    <td colspan="1">13<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>                
+                    <td colspan="1">14<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>         
+                    <td colspan="1">15<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>
                 </tr>
                 <tr>
                 </tr>
                 <tr>                   
-                    <td colspan="1">16<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>       
-                    <td colspan="1">17<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>                
-                    <td colspan="1">18<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>                
-                    <td colspan="1">19<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>         
-                    <td colspan="1">20<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>
+                    <td colspan="1">16<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>       
+                    <td colspan="1">17<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>                
+                    <td colspan="1">18<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>                
+                    <td colspan="1">19<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>         
+                    <td colspan="1">20<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>
                 </tr>
                 <tr>
                 </tr>
                 <tr>                   
-                    <td colspan="1">21<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>       
-                    <td colspan="1">22<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>                
-                    <td colspan="1">23<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>                
-                    <td colspan="1">24<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>         
-                    <td colspan="1">25<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>
+                    <td colspan="1">21<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>       
+                    <td colspan="1">22<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>                
+                    <td colspan="1">23<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>                
+                    <td colspan="1">24<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>         
+                    <td colspan="1">25<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>
                 </tr>
                 <tr>
                 </tr>
                 <tr>                   
-                    <td colspan="1">26<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>       
-                    <td colspan="1">27<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>                
-                    <td colspan="1">28<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>                
-                    <td colspan="1">29<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>         
-                    <td colspan="1">30<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>
+                    <td colspan="1">26<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>       
+                    <td colspan="1">27<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>                
+                    <td colspan="1">28<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>                
+                    <td colspan="1">29<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>         
+                    <td colspan="1">30<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>
                 </tr>
                 <tr>
                 </tr>
                 <tr>                   
-                    <td colspan="1">31<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>       
-                    <td colspan="1">32<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>                
-                    <td colspan="1">33<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>                
-                    <td colspan="1">34<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>         
-                    <td colspan="1">35<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>
+                    <td colspan="1">31<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>       
+                    <td colspan="1">32<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>                
+                    <td colspan="1">33<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>                
+                    <td colspan="1">34<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>         
+                    <td colspan="1">35<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>
                 </tr>
                 <tr>
                 </tr>
                 <tr>                   
-                    <td colspan="1">36<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>       
-                    <td colspan="1">37<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>                
-                    <td colspan="1">38<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>                
-                    <td colspan="1">39<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>         
-                    <td colspan="1">40<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>
+                    <td colspan="1">36<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>       
+                    <td colspan="1">37<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>                
+                    <td colspan="1">38<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>                
+                    <td colspan="1">39<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>         
+                    <td colspan="1">40<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>
                 </tr>
                 <tr>
                 </tr>
                 <tr>                   
-                    <td colspan="1">41<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>       
-                    <td colspan="1">42<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>                
-                    <td colspan="1">43<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>                
-                    <td colspan="1">44<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>         
-                    <td colspan="1">45<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>
+                    <td colspan="1">41<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>       
+                    <td colspan="1">42<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>                
+                    <td colspan="1">43<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>                
+                    <td colspan="1">44<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>         
+                    <td colspan="1">45<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>
                 </tr>
                 <tr>
                 </tr>
                 <tr>                   
-                    <td colspan="1">46<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>       
-                    <td colspan="1">47<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>                
-                    <td colspan="1">48<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>                
-                    <td colspan="1">49<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>         
-                    <td colspan="1">50<input type="setno" id="setno" name="setno[]" min="0" max="50" value="0" ></td>
+                    <td colspan="1">46<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>       
+                    <td colspan="1">47<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>                
+                    <td colspan="1">48<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>                
+                    <td colspan="1">49<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>         
+                    <td colspan="1">50<input type="demand" id="demand" name="demand[]" min="0" max="50" value="0" ></td>
                 </tr>
                 
 
