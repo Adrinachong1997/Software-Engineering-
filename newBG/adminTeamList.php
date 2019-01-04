691aff6f15401295c6ed2b7a6457c025e01adef5
nwgosick0523 <chinglung0523@gmail.com>
2019-01-04 12:03:48 +0800

    新增功能-可查看房間狀態

--- newBG/adminTeamList.php
+++ newBG/adminTeamList.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="style.css" />
+        <link rel="stylesheet" type="text/css" href="adminTeamList.css" />
         <meta charset="UTF-8" />
         <meta name="keywords" content=" " />
         <meta name="description" content=" " />
@@ -13,34 +13,35 @@
         <table id="main">
             <form id="team" method="post" action="?.php" accept-charset="utf-8">
                 <tr>
-                    <td colspan="6" id="background"><font size="6">已存在的房間</font></td>
+                    <td colspan="7" id="background"><font size="6">已存在的房間</font></td>
                 </tr>  
                 <tr>
-                    <td colspan="6" id="background"><font size="4">
+                    <td colspan="7" id="background"><font size="4">
                         管理員 : <?php require("dbconfig.php");
                                 echo $_SESSION['id'];?></font>
                     </td>
                 </tr> 
                 <tr>
-                    <td width="20px">團隊名稱</td>
+                    <td width="20px">房間名稱</td>
                     <td width="20px">工廠</br>Factory</td>
                     <td width="20px">大盤商</br>Distributer</td>
                     <td width="20px">批發商</br>Wholesale</td>
                     <td width="20px">零售商</br>Retailer</td>
-                    <td width="20px">房間刪除</td>
+                    <td width="20px">房間狀態</td>
+                    <td width="20px">刪除</td>
                 </tr> 
                 <?php
                     require('gameModel.php');
                     adminShowTeam()
                 ?>
-                <tr><td colspan="6"></td></tr>
+                <tr><td colspan="7"></td></tr>
                 <tr>
-                    <td colspan="6">
+                    <td colspan="7">
                         <a id="button" href="50thSetting.php">設定五十期需求</a>
                     </td>
                 </tr>
                 <tr>
-                    <td colspan="6"> 
+                    <td colspan="7"> 
                         <a id="button" href="edit.php">編輯個人資料</a>
                         <a id="button" href="loginOut.php">登出</a>
                     </td>
