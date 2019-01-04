7a16be6bec9653047c4b6d8381c2bcf72ca2bdfa
nwgosick0523 <chinglung0523@gmail.com>
2019-01-04 12:03:57 +0800

    新增功能-可查看房間狀態

--- newBG/teamList.php
+++ newBG/teamList.php
@@ -14,35 +14,36 @@
         <table id="main">
             <form id="team" method="post" action="teamUpdate.php" accept-charset="utf-8">
                 <tr>
-                    <td colspan="5" id="background"><font size="6">已存在的房間</font></td>
+                    <td colspan="6" id="background"><font size="6">房間列表</font></td>
                 </tr>
                 <tr>
-                    <td colspan="5" id="background"><font size="4">
+                    <td colspan="6" id="background"><font size="4">
                         用戶名 : <?php require("dbconfig.php");
                                 echo $_SESSION['id'];?></font>
                     </td>
                 </tr>
                 <tr>
-                    <td width="20px">團隊名稱</td>
+                    <td width="20px">房間名稱</td>
                     <td width="20px">工廠</br>Factory</td>
                     <td width="20px">大盤商</br>Distributer</td>
                     <td width="20px">批發商</br>Wholesale</td>
                     <td width="20px">零售商</br>Retailer</td>
+                    <td width="20px">房間狀態</td>
                 </tr>
                 <?php 
                     //列出現有可加入隊伍
                     require('gameModel.php');
                     showTeam();
                 ?>
-                <tr><td colspan="5"></td></tr>
+                <tr><td colspan="6"></td></tr>
                 <tr>
-                    <td colspan="5">
+                    <td colspan="6">
                         <a id="button" href="createTeam.php">建立隊伍</a>
                         <input id="button" type="submit" name="Submit" value="送出" />
                     </td>
                 </tr>
                 <tr>
-                    <td colspan="5">
+                    <td colspan="6">
                         <a id="button" href="edit.php">編輯個人資料</a>
                         <a id="button" href="loginOut.php">登出</a>
                     </td>
