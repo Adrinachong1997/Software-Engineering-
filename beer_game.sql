49c05e37a69e7f2a4e6ac488367d21ac5505ce63
nwgosick0523 <chinglung0523@gmail.com>
2019-01-04 10:39:16 +0800

    整合模組一、二、三的sql

--- beer_game.sql
+++ beer_game.sql
@@ -2,8 +2,8 @@
 -- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- 主機: 127.0.0.1
--- 產生時間： 2019 年 01 月 04 日 03:22
+-- 主機: localhost
+-- 產生時間： 2019 年 01 月 04 日 03:38
 -- 伺服器版本: 10.1.37-MariaDB
 -- PHP 版本： 7.3.0
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- 資料庫： `beer_game`
+-- 資料庫： `test1`
 --
 
 -- --------------------------------------------------------
@@ -38,56 +38,56 @@ CREATE TABLE `gamecycle` (
 --
 
 INSERT INTO `gamecycle` (`week`, `demand`) VALUES
-(1, 44),
-(2, 23),
-(3, 43),
-(4, 32),
-(5, 9),
-(6, 41),
-(7, 37),
-(8, 1),
-(9, 11),
-(10, 12),
-(11, 31),
-(12, 40),
-(13, 19),
-(14, 40),
-(15, 24),
-(16, 20),
-(17, 36),
-(18, 34),
-(19, 33),
-(20, 33),
-(21, 27),
-(22, 24),
-(23, 27),
-(24, 3),
-(25, 24),
-(26, 10),
-(27, 17),
-(28, 3),
-(29, 25),
-(30, 18),
-(31, 9),
-(32, 25),
-(33, 4),
-(34, 24),
-(35, 4),
-(36, 18),
-(37, 37),
-(38, 9),
-(39, 5),
-(40, 30),
-(41, 25),
-(42, 48),
-(43, 16),
-(44, 1),
-(45, 35),
-(46, 18),
-(47, 47),
-(48, 15),
-(49, 44),
-(50, 3);
+(1, 4),
+(2, 11),
+(3, 2),
+(4, 10),
+(5, 5),
+(6, 20),
+(7, 11),
+(8, 14),
+(9, 20),
+(10, 11),
+(11, 7),
+(12, 1),
+(13, 2),
+(14, 11),
+(15, 8),
+(16, 11),
+(17, 8),
+(18, 15),
+(19, 1),
+(20, 15),
+(21, 3),
+(22, 15),
+(23, 3),
+(24, 14),
+(25, 10),
+(26, 6),
+(27, 8),
+(28, 14),
+(29, 17),
+(30, 10),
+(31, 15),
+(32, 12),
+(33, 17),
+(34, 11),
+(35, 19),
+(36, 4),
+(37, 5),
+(38, 14),
+(39, 18),
+(40, 9),
+(41, 18),
+(42, 4),
+(43, 8),
+(44, 15),
+(45, 6),
+(46, 4),
+(47, 14),
+(48, 16),
+(49, 4),
+(50, 8);
 
 -- --------------------------------------------------------
 
@@ -186,77 +186,27 @@ INSERT INTO `player_status` (`id`, `tname`, `week`, `p1`, `p2`, `p3`, `p4`, `sta
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `stu_tel`
---
-
-CREATE TABLE `stu_tel` (
-  `no` int(11) NOT NULL,
-  `uid` char(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `sid` char(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `email` char(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `tel` int(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- 資料表的匯出資料 `stu_tel`
---
-
-INSERT INTO `stu_tel` (`no`, `uid`, `sid`, `email`, `tel`) VALUES
-(5, '陳浩鋐', '105213060', 's105213060@mail1.ncnu.edu.tw', 912345342),
-(6, '蕭泓恩', '105213014', 's105213014@mail1.ncnu.edu.tw', 912345365),
-(7, '宋晨', '105213057', 's105123057@mail1.ncnu.edu.tw', 982734534),
-(8, '香荏彬', '105213069', 's105213069@mail1.ncnu.edu.tw', 912342342),
-(9, '許中昱', '105213002', 's105213002@mail1.ncnu.edu.tw', 988745342),
-(10, '何宏歷', '105213075', 's105213075@mail1.ncnu.edu.tw', 912342341),
-(11, 'asd', '123', 'asd@ad', 123);
-
--- --------------------------------------------------------
-
---
--- 資料表結構 `team`
---
-
-CREATE TABLE `team` (
-  `tno` int(11) NOT NULL,
-  `tname` varchar(30) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- 資料表的匯出資料 `team`
---
-
-INSERT INTO `team` (`tno`, `tname`) VALUES
-(1, 'asd'),
-(2, 'asddd'),
-(3, 'asddd'),
-(4, 'asddd'),
-(5, 'asddd'),
-(6, '123'),
-(7, '123'),
-(8, '123');
-
--- --------------------------------------------------------
-
---
 -- 資料表結構 `tgame`
 --
 
 CREATE TABLE `tgame` (
   `serno` int(11) NOT NULL,
   `tname` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `r1` int(11) NOT NULL,
-  `r2` int(11) NOT NULL,
-  `r3` int(11) NOT NULL,
-  `r4` int(11) NOT NULL
+  `r1` varchar(11) CHARACTER SET latin1 DEFAULT NULL,
+  `r2` varchar(11) CHARACTER SET latin1 DEFAULT NULL,
+  `r3` varchar(11) CHARACTER SET latin1 DEFAULT NULL,
+  `r4` varchar(11) CHARACTER SET latin1 DEFAULT NULL,
+  `go` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- 資料表的匯出資料 `tgame`
 --
 
-INSERT INTO `tgame` (`serno`, `tname`, `r1`, `r2`, `r3`, `r4`) VALUES
-(1, 'test', 1, 1, 0, 0),
-(2, 'test2', 1, 0, 0, 1);
+INSERT INTO `tgame` (`serno`, `tname`, `r1`, `r2`, `r3`, `r4`, `go`) VALUES
+(50, 'uuu', 'im', 'im123', '1', 'im', 1),
+(53, 'qqq', '1', NULL, NULL, NULL, 0),
+(55, 'u777', NULL, NULL, NULL, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -268,20 +218,24 @@ CREATE TABLE `user` (
   `id` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
   `mail` varchar(30) NOT NULL,
-  `sort` int(10) NOT NULL DEFAULT '1'
+  `sort` int(10) NOT NULL DEFAULT '1',
+  `admin` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- 資料表的匯出資料 `user`
 --
 
-INSERT INTO `user` (`id`, `password`, `mail`, `sort`) VALUES
-('1', '1', '', 1),
-('a', '1', '', 1),
-('b', 'a', '', 0),
-('test', 'test', '', 1),
-('test2', 'test2', '', 1),
-('test3', 'test3', '', 1);
+INSERT INTO `user` (`id`, `password`, `mail`, `sort`, `admin`) VALUES
+('1', '1', '', 1, 0),
+('a', '1', '', 1, 0),
+('admin1', '321', 'admin@admin.com', 1, 1),
+('b', 'a', '', 0, 0),
+('im', '123', 'asd@123', 1, 0),
+('im123', '123', 'asd@ads', 1, 0),
+('test', 'test', '', 1, 0),
+('test2', 'test2', '', 1, 0),
+('test3', 'test3', '', 1, 0);
 
 --
 -- 已匯出資料表的索引
@@ -316,18 +270,6 @@ ALTER TABLE `player_record`
 --
 ALTER TABLE `player_status`
   ADD PRIMARY KEY (`id`);
-
---
--- 資料表索引 `stu_tel`
---
-ALTER TABLE `stu_tel`
-  ADD PRIMARY KEY (`no`);
-
---
--- 資料表索引 `team`
---
-ALTER TABLE `team`
-  ADD PRIMARY KEY (`tno`);
 
 --
 -- 資料表索引 `tgame`
@@ -370,22 +312,10 @@ ALTER TABLE `player_status`
   MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- 使用資料表 AUTO_INCREMENT `stu_tel`
---
-ALTER TABLE `stu_tel`
-  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- 使用資料表 AUTO_INCREMENT `team`
---
-ALTER TABLE `team`
-  MODIFY `tno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
 -- 使用資料表 AUTO_INCREMENT `tgame`
 --
 ALTER TABLE `tgame`
-  MODIFY `serno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `serno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
